7275d51ff992f8895b503c51d99152c51cbe65cb
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-12-07 01:02:39 +0100

    chore: copy/paste vanilla-extract plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react";
 import ssr from "vite-plugin-ssr/plugin";
 import { UserConfig } from "vite";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+// import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { vanillaExtractPlugin } from "./ve-plugin";
 import Inspect from "vite-plugin-inspect";
 
 const config: UserConfig = {
